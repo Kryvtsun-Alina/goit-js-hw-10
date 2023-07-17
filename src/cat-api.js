f048c34164a4835058ff06ed34837acc41334d08
Kryvtsun Alina <krivtsun1002@gmail.com>
2023-07-18 01:51:14 +0300

    create markup

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,22 +1,25 @@
 import axios from "axios";
+import Notiflix from "notiflix";
 
-axios.defaults.headers.common["x-api-key"] = "live_jAPlXiMJLi84T0aM7kAVrLCi2yGw3DPISDGS3Wwn43bYAlP3MAZUp4ZAfNTWzhmI";
-const API_KEY =
-  'live_jAPlXiMJLi84T0aM7kAVrLCi2yGw3DPISDGS3Wwn43bYAlP3MAZUp4ZAfNTWzhmI';
-const BASE_URL = 'https://api.thecatapi.com/v1';
+const API_KEY = "live_jAPlXiMJLi84T0aM7kAVrLCi2yGw3DPISDGS3Wwn43bYAlP3MAZUp4ZAfNTWzhmI";
+axios.defaults.headers.common["x-api-key"] = API_KEY;
+
+const BASE_URL = "https://api.thecatapi.com/v1";
 
 export function fetchBreeds() {
-    return axios.get("https://api.thecatapi.com/v1/breeds")
-      .then(response => response.data)
-      .catch(error => {
-        Notiflix.Notify.failure(error.textContent);
-      });
-  }
-  export function fetchCatByBreed(breedId) {
-    return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-      .then(response => response.data)
-      .catch(error => {
-        Notiflix.Notify.failure(error.textContent);
-      });
-  }
-  
+  return axios
+    .get(`${BASE_URL}/breeds`)
+    .then(response => response.data)
+    .catch(error => {
+      Notiflix.Notify.failure(error.textContent);
+    });
+}
+
+export function fetchCatByBreed(breedId) {
+  return axios
+    .get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
+    .then(response => response.data)
+    .catch(error => {
+      Notiflix.Notify.failure(error.textContent);
+    });
+};
--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import axios from "axios";
 import SlimSelect from "slim-select";
 import Notiflix from "notiflix";
-
 import { fetchBreeds, fetchCatByBreed } from "./cat-api";
 
 const refs = {
-    select: document.querySelector('.breed-select'),
-    catInfo: document.querySelector('.cat-info'),
-    loader: document.querySelector('.loader'),
-    error: document.querySelector('.error'),
-  };
-  const { select, catInfo, loader, error } = refs;
+  select: document.querySelector('.breed-select'),
+  catInfo: document.querySelector('.cat-info'),
+  loader: document.querySelector('.loader'),
+  error: document.querySelector('.error'),
+};
+const { select, catInfo, loader, error } = refs;
 
 select.classList.add('hidden');
 error.classList.add('hidden');
@@ -18,16 +17,55 @@ loader.style.display = 'block';
 
 fetchBreeds()
   .then(breeds => onSelected(breeds))
-  .catch(error);
+  .catch(onError);
 
-  function onSelected(breeds) {
-    const options = breeds.map(({ id, name }) => ({
-      text: name,
-      value: id,
-    }));
-    const slimSelect = new SlimSelect({
-      select: select,
-    });
-    slimSelect.setData(options);
-    loader.style.display = 'none';
+function onSelected(breeds) {
+  const options = breeds.map(({ id, name }) => ({
+    text: name,
+    value: id,
+  }));
+  const slimSelect = new SlimSelect({
+    select: select,
+  });
+  slimSelect.setData(options);
+  loader.style.display = 'none';
+}
+
+function onError(err) {
+  loader.style.display = 'none';
+  Notiflix.Notify.failure(error.textContent);
+}
+
+let isFirstLoad = true;
+select.addEventListener('change', e => {
+  if (isFirstLoad) {
+    return (isFirstLoad = false);
   }
+  catInfo.innerHTML = '';
+  loader.style.display = 'block';
+  const breedId = select.value;
+
+  fetchCatByBreed(breedId)
+    .then(cats => {
+      loader.style.display = 'none';
+      const catMarkup = createCatMarkup(cats);
+      catInfo.insertAdjacentHTML('beforeend', catMarkup);
+    })
+    .catch(onError);
+});
+
+function createCatMarkup(cats) {
+  return cats
+    .map(cat => {
+      return `
+        <img class="picture" src="${cat.url}" width="360" />
+        <div class="cat-container">
+            <h1 class="title">${cat.breeds[0].name}</h1>
+            <p class="desc">${cat.breeds[0].description}</p>
+            <p class="temper"><b>Temperament: </b>${cat.breeds[0].temperament}</p>
+        </div>
+      `;
+    })
+    .join('');
+}
+
