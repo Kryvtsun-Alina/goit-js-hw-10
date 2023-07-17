b03e40ea95a7aa5dff60349d91b77d28789d62fb
Kryvtsun Alina <krivtsun1002@gmail.com>
2023-07-18 00:53:28 +0300

    add refs

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ import SlimSelect from "slim-select";
 import Notiflix from "notiflix";
 
 import { fetchBreeds, fetchCatByBreed } from "./cat-api";
+
+const refs = {
+    select: document.querySelector('.breed-select'),
+    catInfo: document.querySelector('.cat-info'),
+    loader: document.querySelector('.loader'),
+    error: document.querySelector('.error'),
+  };
+  const { select, catInfo, loader, error } = refs;
